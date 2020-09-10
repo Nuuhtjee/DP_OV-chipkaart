34fae08dd671c230e47ce7480bac6be4bcb8b23d
Nuuhtjee <nuuh.dinle@gmail.com>
2020-09-10 17:29:23 +0200

    P4 Afgemaakt

---
+++ src/P4/DAO/AdresDAOPsql.java
@@ -0,0 +1,115 @@
+package P4.DAO;
+
+import P4.Domein.Reiziger;
+import P4.Domein.Adres;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdresDAOPsql implements AdresDAO {
+
+    private Connection conn;
+    private ReizigerDAO rdao;
+
+    public AdresDAOPsql(Connection conn){
+        this.conn = conn;
+    }
+
+    public void setRdao(ReizigerDAO rdao) {
+        this.rdao = rdao;
+    }
+
+    @Override
+    public boolean save(Adres adres) {
+        String query = "INSERT into adres (adres_id, postcode, huisnummer,straat, woonplaats,reiziger_id) VALUES (?,?,?,?,?,?)";
+        boolean result = false;
+        try {
+            PreparedStatement pst = conn.prepareStatement(query);
+            pst.setInt(1,adres.getId());
+            pst.setString(2,adres.getPostcode());
+            pst.setString(3,adres.getHuisnummer());
+            pst.setString(4,adres.getStraat());
+            pst.setString(5,adres.getWoonplaats());
+            pst.setInt(6,adres.getReiziger().getReiziger_id());
+            result = pst.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Het maken van een nieuwe adres is mislukt");
+        }
+        return result;
+    }
+
+    @Override
+    public boolean update(Adres adres) {
+        String query = "Update adres set adres_id = ?, postcode = ?, huisnummer = ?, straat = ?, woonplaats = ?, reiziger_id = ? where adres_id = ?";
+        boolean result = false;
+        try {
+            PreparedStatement pst = conn.prepareStatement(query);
+            pst.setInt(1,adres.getId());
+            pst.setString(2,adres.getPostcode());
+            pst.setString(3,adres.getHuisnummer());
+            pst.setString(4,adres.getStraat());
+            pst.setString(5,adres.getWoonplaats());
+            pst.setInt(6,adres.getReiziger().getReiziger_id());
+            pst.setInt(7,adres.getId());
+
+            result = pst.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Het updaten van het adres is mislukt");
+        }
+        return result;
+    }
+
+    @Override
+    public boolean delete(Adres adres) {
+        String query = "Delete from adres where adres_id = ?";
+        boolean result = false;
+        try {
+            PreparedStatement pst = conn.prepareStatement(query);
+            pst.setInt(1,adres.getId());
+            result = pst.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Het verwijderen van het adres is mislukt");
+        }
+        return result;
+    }
+
+    @Override
+    public Adres findByReiziger(Reiziger reiziger) {
+        String query = "Select * from adres where reiziger_id = ?";
+        Adres adres = null;
+        try {
+            PreparedStatement pst = conn.prepareStatement(query);
+            pst.setInt(1,reiziger.getReiziger_id());
+            ResultSet rs = pst.executeQuery();
+            while(rs.next()){
+                adres = new Adres(rs.getInt(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5), reiziger);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return adres;
+    }
+
+    @Override
+    public List<Adres> findAll() {
+        String query = "Select * from adres";
+        List<Adres> adres = new ArrayList<>();
+        try {
+            PreparedStatement pst = conn.prepareStatement(query);
+            ResultSet rs = pst.executeQuery();
+            while(rs.next()){
+                adres.add(new Adres(rs.getInt(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5),rdao.findById(rs.getInt(6))));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return adres;
+    }
+}
---
+++ src/P4/DAO/ReizigerDAOPsql.java
@@ -0,0 +1,139 @@
+package P4.DAO;
+
+import P4.Domein.Reiziger;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReizigerDAOPsql implements ReizigerDAO {
+
+
+    private Connection conn;
+    private AdresDAO adao;
+    private OVChipkaartDAO odao;
+
+
+    public ReizigerDAOPsql(Connection conn){
+        this.conn = conn;
+    }
+
+    public void setAdao(AdresDAO adao) {
+        this.adao = adao;
+    }
+
+    public void setOdao(OVChipkaartDAO odao) {
+        this.odao = odao;
+    }
+
+    @Override
+    public boolean save(Reiziger reiziger) {
+        String query = "INSERT INTO reiziger (reiziger_id,voorletters,tussenvoegsel,achternaam,geboortedatum) VAlUES (?,?,?,?,?)";
+        boolean result = false;
+        try {
+            PreparedStatement pst = conn.prepareStatement(query);
+            pst.setInt(1,reiziger.getReiziger_id());
+            pst.setString(2,reiziger.getVoorletters());
+            pst.setString(3,reiziger.getTussenvoegsel());
+            pst.setString(4,reiziger.getAchternaam());
+            pst.setDate(5, (Date) reiziger.getGeboortedatum());
+            result = pst.execute();
+        } catch (SQLException e) {
+            System.out.println("Het maken van een nieuwe reiziger en opslaan in de database is mislukt");
+        }
+        return result;
+    }
+
+    @Override
+    public boolean update(Reiziger reiziger) {
+        String query = "UPDATE reiziger  set reiziger_id = ?, voorletters = ?, tussenvoegsel = ? , achternaam = ?, geboortedatum = ? where reiziger_id = ?";
+        boolean result = false;
+        try{
+            PreparedStatement pst = conn.prepareStatement(query);
+            pst.setInt(1,reiziger.getReiziger_id());
+            pst.setString(2,reiziger.getVoorletters());
+            pst.setString(3,reiziger.getTussenvoegsel());
+            pst.setString(4,reiziger.getAchternaam());
+            pst.setDate(5, (Date) reiziger.getGeboortedatum());
+            pst.setInt(6,reiziger.getReiziger_id());
+            result = pst.execute();
+    } catch (SQLException e) {
+            System.out.println("Het updaten van een reiziger is mislukt");
+    }
+        return result;
+    }
+
+    @Override
+    public boolean delete(Reiziger reiziger) {
+        String query = "DELETE from reiziger where reiziger_id = ?";
+        boolean result = false;
+        try{
+            PreparedStatement pst = conn.prepareStatement(query);
+            pst.setInt(1,reiziger.getReiziger_id());
+            result = pst.execute();
+        } catch (SQLException e) {
+            System.out.println("Het verwijderen van een reiziger is mislukt");
+        }
+        return result;
+    }
+
+    @Override
+    public Reiziger findById(int id) {
+        String query = "SELECT * From reiziger where reiziger_id=?";
+        Reiziger temp = null;
+        try {
+            PreparedStatement pst = conn.prepareStatement(query);
+            pst.setInt(1,id);
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()){
+                temp = new Reiziger(rs.getInt(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getDate(5),null);
+                temp.setAdres(adao.findByReiziger(temp));
+                temp.setOVchipkaarten(odao.findByReiziger(temp));
+            }
+        } catch (SQLException e) {
+            System.out.println("Het vinden van een reiziger met het gegeven ID is mislukt");
+        }
+        return temp;
+    }
+
+    @Override
+    public List<Reiziger> findByGbdatum(String datum) {
+        String query = "SELECT * From reiziger where geboortedatum= ?";
+        List<Reiziger> reizigers = new ArrayList<>();
+        try {
+            PreparedStatement pst = conn.prepareStatement(query);
+            pst.setDate(1, Date.valueOf(datum));
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()){
+                Reiziger temp = new Reiziger(rs.getInt(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getDate(5),null);
+                temp.setAdres(adao.findByReiziger(temp));
+                temp.setOVchipkaarten(odao.findByReiziger(temp));
+                reizigers.add(temp);
+            }
+        } catch (SQLException e) {
+            System.out.println("Het vinden van een reiziger met het gegeven geboortedatum is mislukt");
+
+        }
+        return reizigers;
+    }
+
+    @Override
+    public List<Reiziger> findAll() {
+        String query = "SELECT * From reiziger";
+        List<Reiziger> reizigers = new ArrayList<>();
+        try {
+            PreparedStatement pst = conn.prepareStatement(query);
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()){
+                Reiziger temp = new Reiziger(rs.getInt(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getDate(5),null);
+                temp.setAdres(adao.findByReiziger(temp));
+                temp.setOVchipkaarten(odao.findByReiziger(temp));
+                reizigers.add(temp);
+
+            }
+        } catch (SQLException e) {
+            System.out.println("Het vinden van alle reizigers is mislukt");
+        }
+        return reizigers;
+    }
+}
